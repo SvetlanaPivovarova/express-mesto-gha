c8cf2f81421ad4c43c5b6a75df5a0e57771b1e81
Svetlana A. Pivovarova <pivovarova.svetlana@gmail.com>
2022-06-15 08:11:09 +0400

    создать приложение на express, создать модель пользователя

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+// Слушаем 3000 порт
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+// подключаемся к серверу mongo
+mongoose.connect('mongodb://localhost:27017/mydb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+app.listen(PORT, () => {
+  // Если всё работает, консоль покажет, какой порт приложение слушает
+  console.log(`App listening on port ${PORT}`)
+})
