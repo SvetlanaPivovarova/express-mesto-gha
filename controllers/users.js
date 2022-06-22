a0ee1d9d331b63cbea59f0f697369e0c755ad0dd
Svetlana A. Pivovarova <pivovarova.svetlana@gmail.com>
2022-06-22 11:10:33 +0400

    исправить ошибки get user

--- controllers/users.js
+++ controllers/users.js
@@ -47,9 +47,9 @@ const updateUserProfile = (req, res) => {
   User.findByIdAndUpdate(req.params._id, { name, about })
     .then((user) => {
       if (!user) {
-        return res.status(404).send({ message: 'Пользователь с указанным _id не найден' });
+        return res.status(400).send({ message: 'Пользователь с указанным _id не найден' });
       }
-      return res.send({ data: user });
+      return res.status(200).send({ data: user });
     })
     .catch((err) => {
       if (err.name === 'ValidationError') {
